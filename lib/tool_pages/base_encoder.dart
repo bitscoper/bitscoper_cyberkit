66260c7a9915a67a708284e7616ec3483e0e0747
Abdullah As-Sadeed <bitscoper@tutanota.com>
2026-04-05 14:17:42 +0600

    Upgraded Versions of GitHub Actions; and Added Missing Changes of `0d96f8ba331be664331d78ae7844910ffefcb7a1` Commit.

--- lib/tool_pages/base_encoder.dart
+++ lib/tool_pages/base_encoder.dart
@@ -245,17 +245,7 @@ class BaseEncoderPageState extends State<BaseEncoderPage> {
               trailing: IconButton(
                 icon: const Icon(Icons.copy_rounded),
                 onPressed: () {
-                  try {
-                    copyToClipboard(context, entry.key, result);
-                  } catch (error) {
-                    debugPrint(error.toString());
-
-                    showMessageDialog(
-                      context,
-                      AppLocalizations.of(context)!.error,
-                      error.toString(),
-                    );
-                  } finally {}
+                  copyToClipboard(context, entry.key, result);
                 },
                 tooltip: AppLocalizations.of(context)!.copy_to_clipboard,
               ),
