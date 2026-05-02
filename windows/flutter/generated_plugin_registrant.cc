085820e1c4140fd55a96b5797fd96d3b32ca2c71
Abdullah As-Sadeed <abdullah_as-sadeed.bd@protonmail.com>
2026-05-02 13:19:30 +0600

    Replaced Usage of Specific Font with System Font; Enabled `useMaterial3` & `useSystemColors` in `ThemeData`; `flutter pub upgrade`; and Formatted Files.

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,7 +7,6 @@
 #include "generated_plugin_registrant.h"
 
 #include <connectivity_plus/connectivity_plus_windows_plugin.h>
-#include <dynamic_color/dynamic_color_plugin_c_api.h>
 #include <file_saver/file_saver_plugin.h>
 #include <flutter_blue_plus_winrt/flutter_blue_plus_plugin.h>
 #include <nsd_windows/nsd_windows_plugin_c_api.h>
@@ -17,8 +16,6 @@
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   ConnectivityPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
-  DynamicColorPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("DynamicColorPluginCApi"));
   FileSaverPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSaverPlugin"));
   FlutterBluePlusPluginRegisterWithRegistrar(
